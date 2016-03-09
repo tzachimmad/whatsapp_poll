aa1753ece7a2b9fb7ba9e0e91592fabcb2dca457
tzachimmad <lapidotmmad@aim.com>
2016-03-09 14:45:13 +0200

    Added files via upload

---
+++ whatsapp_parser.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+from collections import defaultdict
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def comming (output_file, msg):
+    come = 1
+    friend = 0
+    if "לא" in msg[0]:
+        come = 0
+    for post in msg:
+        if (post == msg[0]) or ("Air" in post):
+            continue
+        if "חבר" in post:
+            if "בא" in post:
+                friend = 1
+            if "מגיע" in post:
+                friend = 1
+            if "מביא" in post:
+                friend = 1
+            if ("לא" or "מישהו" or "מי" or "?") in post:
+                friend = 0
+        if ("אבוא" or "אגיע") in post:
+            if "לא" not in post:
+                come = 1
+            else:
+                come = 0
+        if ("בא" or "יגיע" or "יבוא") in post:
+            if "אני" in post:
+                come = 1
+                if "לא" in post:
+                    come = 0
+            else:
+                friend = 1
+                if ("לא" or "מי" or "מישהו" or "?") in post:
+                    friend = 0
+    output_file.write(str(come))
+    output_file.write(",")
+    output_file.write(str(friend))
+    output_file.write(",")
+    return come,friend
+
+
+def print_to_csv(output_file,key,value):
+    coming,friend = comming(output_file,value)
+    output_file.write(key)
+    output_file.write(",")
+    for item in value:
+        output_file.write(str(item))
+        output_file.write(",")
+    output_file.write("\n")
+    return coming,friend
+
+def print_header(output_file):
+    output_file.write("מגיע")
+    output_file.write(",")
+    output_file.write("מביא חבר")
+    output_file.write(",")
+    output_file.write("שחקן")
+    output_file.write("\n")
+
+def fill_dico(dico):
+    print "Enter text please, after finished press enter and then ctrl+d (ctr+z on windows)\n"
+    text = sys.stdin.read()
+    key = "defult"
+    for line in text.splitlines():
+        if "]" in line:
+            tmp = line.split ("]")
+            key_with_msg = tmp[1].split(":")
+            key = key_with_msg[0]
+            msg = key_with_msg[1]
+        else:
+            msg = line
+        dico[key].append(msg)
+
+def create_csv_file():
+    dico = defaultdict(list)
+    fill_dico(dico)
+    output_file = open("output.csv", 'w')
+    print_header(output_file)
+    comma = friends = 0
+    for item in dico.items():
+        cama, haver= print_to_csv(output_file,item[0],item[1])
+        comma += cama
+        friends += haver
+    output_file.write(str(comma)+","+str(friends))
+    output_file.close()
+    print "Done creating local csv file output.csv "
+
+def printer(input):
+    print input
---
+++ whatsapp_poll.py
@@ -0,0 +1,80 @@
+from __future__ import print_function
+import httplib2
+from whatsapp_parser import create_csv_file, printer
+import os
+
+from apiclient import errors
+from apiclient.http import MediaFileUpload
+from apiclient import discovery
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/drive'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Drive API Python Quickstart'
+
+# drive file parameters
+FILE_ID = '1GU5e-RFYRsYL1-YYL3ooMHE2VOHnz09oE8LZrmQa4I0'
+FILE_LINK = 'https://docs.google.com/spreadsheets/d/1GU5e-RFYRsYL1-YYL3ooMHE2VOHnz09oE8LZrmQa4I0/edit?usp=sharing'
+TITLE = 'Matomy Air'
+MIME_TYPE = 'csv'
+#upload file path
+FILE_UPLOAD = 'output.csv'
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'drive-python-quickstart.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+from apiclient import errors
+from apiclient.http import MediaFileUpload
+
+def csv_to_drive():
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('drive', 'v2', http=http)   
+    file = service.files().get(fileId=FILE_ID).execute()
+    media_body = MediaFileUpload(FILE_UPLOAD, mimetype=MIME_TYPE, resumable=True)
+    service.files().update(fileId=FILE_ID,body = {'title':[TITLE],'mime_type':MIME_TYPE},
+        newRevision=1,
+        media_body='output.csv').execute()
+
+def main():
+    create_csv_file()
+    csv_to_drive()
+    printer("Link to file:"+FILE_LINK)
+
+if __name__ == '__main__':
+    main()
